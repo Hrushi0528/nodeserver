c999b903ef7945b430cd406ebc43cb8bc8f5793d
Hrushikesh Dodla <129310243+Hrushi0528@users.noreply.github.com>
2025-01-30 21:46:32 +0530

    Changed UserSchema by adding {Versionkey:false}

--- server1.js
+++ server1.js
@@ -14,7 +14,9 @@ app.use(express.json());
 const userSchema = new mongoose.Schema({
     /*username: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // Store hashed passwords*/
-},{strict:false});
+},{strict:false,
+   versionKey: false
+});
 
 const User = mongoose.model('users', userSchema);
 
