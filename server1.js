a99b58d0df0ceb512a2a5bbca7c95bdffe374638
Hrushikesh Dodla <129310243+Hrushi0528@users.noreply.github.com>
2025-01-31 23:38:07 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -91,7 +91,7 @@ app.post('/users', async (req, res) => {
     }
 });
 
-// Start Server
+// Start Server (optional)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
