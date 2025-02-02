5cb6dcfaa6a336a852040c84b82215bb9153c6e6
Hrushikesh Dodla <129310243+Hrushi0528@users.noreply.github.com>
2025-02-02 19:07:35 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -50,6 +50,7 @@ app.post('/login', async (req, res) => {
    
 
     try {
+        // Find user by email
         const user = await User.findOne({email:username});
         const user1 = await User.findOne({username:username});
         if (!user && !user1) {
@@ -59,7 +60,6 @@ app.post('/login', async (req, res) => {
         if ((!user || user.password !== password) && (!user1 || user1.password!== password)){
             return res.status(400).json({ message: 'Wrong Password' });
         }
-
 
         res.json({ message: 'Login successful' });
     } catch (error) {
@@ -71,31 +71,33 @@ app.post('/login', async (req, res) => {
 app.post('/signup', async (req, res) => {
     try {
         console.log(req.body)
-        const {username,email,pwd,re_pwd}=req.body;
+        const {username,email,password,re_password}=req.body;
         delete req.body.re_password;
 
         const user = new User(req.body);
-        const check_uname= await User.findOne({username:username});
+        const check_uname= await User.findOne({username:username}); 
+        const check_email= await User.findOne({email:email});
         //Checking if the username already exist.
         if (check_uname){
             return res.status(400).json({message:'Username Already Exist',uname:''});
         }
-        const check_email= await User.findOne({email:email});
         //Checking if the email already exist.
         if (check_email){
             return res.status(400).json({message:'Email Already Exist',email:''});
         }
+        console.log(password,re_password);
         if (password !== re_password){
             return res.status(400).json({message:'Password and Re-Enter password does not Match',password:''});
-        }
-        await user.save();
-        res.status(201).send({ message: 'User added successfully'});
+        }   
+            await user.save();
+            res.status(201).send({ message: 'User added successfully'});
     } catch (err) {
         res.status(400).send(err);
     }
 });
 
-// Start Server (optional)
+
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
